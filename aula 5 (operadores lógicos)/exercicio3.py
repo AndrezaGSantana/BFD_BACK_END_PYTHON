3eda2de3aa35a7b6360c9307fef9330b10f68d5e
Andreza Gomes de Santana <102179138+AndrezaGSantana@users.noreply.github.com>
2025-08-20 14:49:38 -0300

    Add files via upload

---
+++ aula 5 (operadores lógicos)/exercicio3.py
@@ -0,0 +1,14 @@
+# Variável
+esta_chovendo = True
+nao_esta_chovendo = False
+
+# Verificação lógica
+esta_chovendo and nao_esta_chovendo
+
+# Saída
+print(f"Está chovendo =", esta_chovendo)
+print(f"Não está chovendo=", nao_esta_chovendo)
+
+# Saída esperada: 
+# Está chovendo = Resultado da operação lógica
+# Não está chovendo = Resultado da opração lógica
