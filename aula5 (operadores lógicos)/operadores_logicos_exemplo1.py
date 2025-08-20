8a7457329223734120ea8689bffb1d2ad17383dd
Andreza Gomes de Santana <102179138+AndrezaGSantana@users.noreply.github.com>
2025-08-20 14:51:32 -0300

    Add files via upload

---
+++ aula5 (operadores lógicos)/operadores_logicos_exemplo1.py
@@ -0,0 +1,15 @@
+a = 10
+b = 3
+
+resultado_and = (a > 5) and (b < 5)
+resultado_or = (a > 15) or (b < 5)
+resultado_not = not (a > 5)
+
+print (f'(a > 5) and (b< 5): {resultado_and}' +
+       f'\n(a > 5) or (b < 5): {resultado_or}' +
+       f'\nnot(a > 5): {resultado_not}')
+
+# Saída esperada: 
+# (a > 5) and (b< 5): Resultado da operação lógica
+# (a > 5) or (b < 5): Resultado da operação lógica
+# not(a > 5): Resultado da operação lógica
